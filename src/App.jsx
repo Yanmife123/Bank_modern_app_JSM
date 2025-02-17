e5bc710d253250641c0219523a0bafb82227982a
Yanmife123 <yanmifeigwe@gmail.com>
2025-02-17 16:12:39 +0100

    My React project with JavascriptMastery but I'm not yet done with the tutorial this is still half project

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import style from "./style";
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  /* Clients, */
+  CTA,
+  Footer,
+} from "./components";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${style.paddingX}  ${style.flexCenter} `}>
+        <div className={`${style.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+      <div className={`bg-primary  ${style.flexStart}`}>
+        <div className={`${style.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      <div className={`bg-primary ${style.paddingX}  ${style.flexStart}`}>
+        <div className={`${style.boxWidth}`}>
+          <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />{" "}
+          {/* <Clients />  */}
+          <CTA /> <Footer />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+import Navbar from "./NavBar";
+import Business from "./Business";
+import Billing from "./Billing";
+import Testimonials from "./Testimonials";
+import GetStarted from "./GetStarted";
+import CTA from "./CTA";
+import Button from "./Buttons";
+import FeedbackCard from "./FeedbackCard";
+import Stats from "./Stats";
+import Hero from "./Hero";
+import Footer from "./Footer";
+import CardDeal from "./CardDeal";
+
+export {
+  Navbar,
+  Business,
+  Billing,
+  Testimonials,
+  GetStarted,
+  CTA,
+  Button,
+  FeedbackCard,
+  Stats,
+  Hero,
+  Footer,
+  CardDeal,
+};
